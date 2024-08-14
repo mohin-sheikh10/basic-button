4d78d8572069093925f63e80fde49eea0b93a0f1
mohin-sheikh10 <mohinshei014@gmail.com>
2024-08-14 21:09:45 +0600

    Added JavaScript

--- script/app.js
+++ script/app.js
@@ -1,4 +1,17 @@
-const btn = document.querySelector(".btn");
+/* const btn = document.querySelector(".btn");
 btn.addEventListener("click", function () {
   btn.style.backgroundColor = "#d35400";
+});
+ */
+
+function generateRGBColor() {
+  const red = Math.floor(Math.random() * 255);
+  const green = Math.floor(Math.random() * 255);
+  const blue = Math.floor(Math.random() * 255);
+  return `rgb(${red},${green},${blue})`;
+}
+
+const btn = document.querySelector(".btn");
+btn.addEventListener("click", function () {
+  btn.style.backgroundColor = generateRGBColor();
 });
